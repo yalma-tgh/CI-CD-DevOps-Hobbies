43cb1b40fcb8fb9b2ad6606c0b5dfcfbfbb1e6c3
yalma-tgh <fateme0ft0@gmail.com>
2024-08-15 15:53:54 +0330

    feat(urls): add users in urls

--- social_media/api/urls.py
+++ social_media/api/urls.py
@@ -2,4 +2,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('blog/', include(('social_media.blog.urls', 'blog')))
+    path('users/', include(('social_media.users.urls', 'users')))
 ]
