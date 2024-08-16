0b9eab9169315e16abb2fd0e380256d9169a03fb
DanyalTaghipor <danyqw.dt@gmail.com>
2024-08-16 23:21:25 +0330

    delete(product_service): delete product service

--- social_media/blog/services/products.py
+++
@@ -1,6 +0,0 @@
-from django.db.models import QuerySet
-from social_media.blog.models import Product
-
-
-def create_product(name: str) -> QuerySet[Product]:
-    return Product.objects.create(name=name)
